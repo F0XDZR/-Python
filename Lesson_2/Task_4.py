3111b3832f99b866a18a44dbfff2fa8c3e53a72e
Kirill_khnyzhov <kirillkhnizhov@gmail.com>
2022-07-19 12:07:14 +0300

    Добавил нумерацию строк

--- Lesson_2/Task_4.py
+++ Lesson_2/Task_4.py
@@ -1,9 +1,10 @@
 user_string = str(input('Введите предложение: '))
-
+count = 0
 new_string = user_string.split()
 
 for word in new_string:
+    count += 1
     if len(word) > 10:
-        print(f'{word[:11]} - Слово длинное. Напечатано только 10 символов')
+        print(f'{count}) {word[:11]} - Слово длинное. Напечатано только 10 символов')
     else:
-        print(f'{word.upper()} - длина слова {len(word)}')
+        print(f'{count}) {word.upper()} - длина слова {len(word)}')
